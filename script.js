8d47ebb7b9a2fe9e30406168b471ac062c074d3a
kaitlyn-ott <89671410+kaitlyn-ott@users.noreply.github.com>
2021-11-24 21:46:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function addition(a,b) {
+	return a + b
+}
+
+let x = addition(7,9);
+console.log(x)
+
+
+function largest(a,b,c) {
+	return Math.max(a,b,c)
+}
+
+let y = largest(25,38,92)
+console.log(y)
+
+function evenOdd(a) {
+	if (a % 2 == 0) {
+		return "even"
+	} else {
+		return "odd"
+	}
+}
+
+let z = evenOdd(767)
+console.log(z)
+
